e01efd0ab07c36b9d11ce91a3759b2eeb277092d
RobertJoefly <32322636+RobertJoefly@users.noreply.github.com>
2021-05-09 02:34:03 -0700

    Problem Description
    Write a code to get the input in the given format and print the output in the given format.
    Input Description:
    A single line contains a string.
    Output Description:
    Print the characters in a string separated by line.
    Sample Input :
    guvigeek
    Sample Output :
    g
    u
    v
    i
    g
    e
    e
    k

---
+++ IO7.py
@@ -0,0 +1,20 @@
+"""Write a code to get the input in the given format and print the output in the given format.
+Input Description:
+A single line contains a string.
+Output Description:
+Print the characters in a string separated by line.
+Sample Input :
+guvigeek
+Sample Output :
+g
+u
+v
+i
+g
+e
+e
+k"""
+
+user = input()
+for i in user:
+    print(i)
